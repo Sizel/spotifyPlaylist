3e533085ca92d0e5e9be6877126d4ba540f615c8
max <xsizelx@gmail.com>
2021-11-07 21:06:12 +0200

    auth_spec.rb: Changed spec's matcher from "outputs to the console" to "raise_error"

--- spec/auth_spec.rb
+++ spec/auth_spec.rb
@@ -1,9 +1,9 @@
-require_relative '../lib/spotify_playlist'
+require_relative '../lib/spotify_playlist/auth'
 
-RSpec.describe SpotifyPlaylist::Auth do
+RSpec.describe Auth do
   context 'given incorrect username or password' do
-    it 'outputs to console that credentials are wrong' do
-      auth = SpotifyPlaylist::Auth.new('wrong_username', 'wrong_password')
+    it 'raises an exception with message: "Incorrect username or password."' do
+      auth = Auth.new('wrong_username', 'wrong_password')
       expect { auth.authorize }.to raise_exception('Incorrect username or password.')
     end
   end
